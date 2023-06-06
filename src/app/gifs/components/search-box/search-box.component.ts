75a4e3332b70c35feb51889acade1a67ed2406aa
yuguito <yugito_is@hotmail.com>
2023-06-05 20:45:45 -0500

    referencia local

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
     template: `
   <h5>Buscar: </h5>
-  <input type="text" class="form-control" placeholder="Buscar gifs...">
+  <input type="text" class="form-control" placeholder="Buscar gifs..."
+  #txtTagInput  
+  (keyup.enter)="searchTag()"
+  >
   `
 })
 
 export class SearchBoxComponent{ 
+  //el viewchild es una referencia local
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() { }
+  //searchTag(newTag: string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+   }
  
 }
